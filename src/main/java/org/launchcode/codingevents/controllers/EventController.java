dc5a1e7a4d589906d40d9ea09d4212e87a07eed5
haley13 <allickhaley@gmail.com>
2021-06-01 16:49:55 -0500

    added table headers

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Chris Bay
@@ -19,12 +21,22 @@ public class EventController {
 
     private static List<String> events = new ArrayList<>();
 
-    @GetMapping
-    public String displayAllEvents(Model model) {
-        model.addAttribute("title", "All Events");
-        model.addAttribute("events", events);
-        return "events/index";
-    }
+   @GetMapping
+   public String displayAllEvents(Model model) {
+       HashMap<String, String> events = new HashMap();
+           events.put("Strange Loop","Conference to developers.");
+           events.put("Code With Pride","LaunchCode LGBTQ+ affinity group");
+           events.put("Apple WWDC","Apple conference");
+
+           return "events/index";
+
+       }
+
+
+//        model.addAttribute("title", "All Events");
+//        model.addAttribute("events", events);
+//        return "events/index";
+    //}
 
     @GetMapping("create")
     public String displayCreateEventForm(Model model) {
