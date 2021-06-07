af194bc9ad0a59ef756db5867e4a666109de7ca0
haley13 <allickhaley@gmail.com>
2021-06-07 11:57:00 -0500

    added validation errors

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -5,8 +5,10 @@ import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -33,10 +35,18 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String processCreateEventForm(@ModelAttribute Event newEvent) {
-        EventData.add(newEvent);
-        return "redirect:";
-    }
+    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent,
+                                         Errors errors, Model model) {
+       if(errors.hasErrors()) {
+           model.addAttribute("title", "Create Event");
+           model.addAttribute("errorMsg", "Bad data!");
+           return "events/create";
+       }
+
+       EventData.add(newEvent);
+       return "redirect:";
+   }
+
     @GetMapping("delete")
     public String displayDeleteEventForm(Model model){
        model.addAttribute("title", "Delete Events");
